a68fd577615ced036a0a71933a12fe9ffab680e0
Gilbert Consellado <gilbertconsellado@gmail.com>
2022-02-27 22:33:02 +0800

    tokyonight border color

--- lua/plugins/config/tokyonight_conf.lua
+++ lua/plugins/config/tokyonight_conf.lua
@@ -1,4 +1,5 @@
 vim.g.tokyonight_style = "night"
 vim.g.tokyonight_italic = true
-vim.g.tokyonight_hide_inactive_statusline = true
+vim.g.tokyonight_colors = { border = "#363d56" }
+vim.g.tokyonight_hide_inactive_statusline = false
 vim.cmd([[colorscheme tokyonight]])
