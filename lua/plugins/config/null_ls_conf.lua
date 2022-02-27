7251bace05e35dbebfd987929cde10b24b4bbbbb
Gilbert Consellado <gilbertconsellado@gmail.com>
2022-02-27 16:42:45 +0800

    add selene

--- lua/plugins/config/null_ls_conf.lua
+++ lua/plugins/config/null_ls_conf.lua
@@ -1,4 +1,4 @@
 vim.cmd([[autocmd BufWritePre *.lua lua vim.lsp.buf.formatting_sync(nil, 100)]])
 
 local null_ls = require("null-ls")
-null_ls.setup({ sources = { null_ls.builtins.formatting.stylua } })
+null_ls.setup({ sources = { null_ls.builtins.formatting.stylua, null_ls.builtins.diagnostics.selene } })
