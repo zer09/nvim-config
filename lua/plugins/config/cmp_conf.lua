9bd92dd3353e0bd3e62f85c442a4e2a60ded2dd4
Gilbert Consellado <gilbertconsellado@gmail.com>
2022-03-01 22:56:57 +0800

    Remove cmp unneeded mapping

--- lua/plugins/config/cmp_conf.lua
+++ lua/plugins/config/cmp_conf.lua
@@ -33,9 +33,6 @@ cmp.setup({
 	mapping = {
 		["<C-p>"] = cmp.mapping.select_prev_item(),
 		["<C-n>"] = cmp.mapping.select_next_item(),
-		["<C-d>"] = cmp.mapping.scroll_docs(-4),
-		-- ["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete(),
 		["<C-e>"] = cmp.mapping.close(),
 		["<CR>"] = cmp.mapping.confirm({
 			behavior = cmp.ConfirmBehavior.Replace,
