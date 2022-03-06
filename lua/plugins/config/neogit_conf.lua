7cf439e5220d320aa6508e114e76a26dbf121f0c
Gilbert Consellado <gilbertconsellado@gmail.com>
2022-03-06 10:01:35 +0800

    Neogit disable insert on commit

--- lua/plugins/config/neogit_conf.lua
+++ lua/plugins/config/neogit_conf.lua
@@ -1,6 +1,6 @@
 require("neogit").setup({
 	disable_commit_confirmation = true,
-	disable_insert_on_commit = false,
+	-- disable_insert_on_commit = false,
 	signs = {
 		-- { CLOSED, OPENED }
 		section = { "", "" },
