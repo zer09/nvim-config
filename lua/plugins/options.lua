99132c5a659d2191bf69ab2f2425f584845dba73
Gilbert Consellado <gilbertconsellado@gmail.com>
2022-03-09 00:27:53 +0800

    Fix base16 telescope color compatibility

--- lua/plugins/options.lua
+++ lua/plugins/options.lua
@@ -18,6 +18,19 @@ conf("aerial")
 -- conf("material")
 -- conf("oceanic_next")
 
-vim.cmd([[colorscheme base16-solarized-dark]])
 -- vim.cmd([[colorscheme base16-monokai]])
 -- vim.cmd([[colorscheme base16-tomorrow]])
+vim.cmd([[colorscheme base16-solarized-dark]])
+
+vim.cmd([[
+highlight! link TelescopeSelection    Visual
+highlight! link TelescopeNormal       Normal
+highlight! link TelescopePromptNormal TelescopeNormal
+highlight! link TelescopeBorder       TelescopeNormal
+highlight! link TelescopePromptBorder TelescopeBorder
+highlight! link TelescopeTitle        TelescopeBorder
+highlight! link TelescopePromptTitle  TelescopeTitle
+highlight! link TelescopeResultsTitle TelescopeTitle
+highlight! link TelescopePreviewTitle TelescopeTitle
+highlight! link TelescopePromptPrefix Identifier
+]])
