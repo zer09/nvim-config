d55c5518245aaec9daaadefc52a512212fc6ef2a
Gilbert Consellado <gilbertconsellado@gmail.com>
2022-03-13 14:43:23 +0800

    Update autopairs config

--- lua/plugins/config/autopairs_conf.lua
+++ lua/plugins/config/autopairs_conf.lua
@@ -1,4 +1,11 @@
-require("nvim-autopairs").setup()
+require("nvim-autopairs").setup({
+	check_ts = true,
+	-- Don't add pairs if it already has a close pair in the same line
+	enable_check_bracket_line = false,
+	-- will ignore alphanumeric and `.` symbol
+	ignored_next_char = "[%w%.]",
+	fastwrap = {},
+})
 
 -- If you want insert `(` after select function or method item
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
