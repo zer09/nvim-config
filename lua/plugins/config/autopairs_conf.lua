d64858b49007ce494ee053fb08469a6b33a38db3
Gilbert Consellado <gilbertconsellado@gmail.com>
2022-03-14 16:52:19 +0800

    auto pairs add rule for angle bracket

--- lua/plugins/config/autopairs_conf.lua
+++ lua/plugins/config/autopairs_conf.lua
@@ -1,4 +1,5 @@
-require("nvim-autopairs").setup({
+local npairs = require("nvim-autopairs")
+npairs.setup({
 	check_ts = true,
 	-- Don't add pairs if it already has a close pair in the same line
 	enable_check_bracket_line = false,
@@ -11,3 +12,11 @@ require("nvim-autopairs").setup({
 local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 local cmp = require("cmp")
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
+
+-- custom rules
+local Rule = require("nvim-autopairs.rule")
+local cond = require("nvim-autopairs.conds")
+
+npairs.add_rules({
+	Rule("<", ">", { "typescript" }):with_pair(cond.not_before_text(" ")),
+})
