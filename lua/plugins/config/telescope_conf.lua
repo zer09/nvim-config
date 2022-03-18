0b5079dd40caaaeb51f8e339dcf904b9dfcc0a72
Gilbert Consellado <gilbertconsellado@gmail.com>
2022-03-18 16:46:18 +0800

    telescope horizontal layout

--- lua/plugins/config/telescope_conf.lua
+++ lua/plugins/config/telescope_conf.lua
@@ -4,7 +4,7 @@ telescope.setup({
 	defaults = {
 		prompt_prefix = "❯ ",
 		selection_caret = "❯ ",
-		layout_strategy = "vertical",
+		-- layout_strategy = "vertical",
 		file_ignore_patterns = { "node_modules/.*", "%.env", "yarn.lock", "package-lock.json" },
 		mappings = require("mappings").telescope_default_mappings,
 	},
