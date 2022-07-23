13acaa8b538c133d6d01b6cc5ff03c151dfe1693
Gilbert Consellado <gilbertconsellado@gmail.com>
2022-07-24 07:55:55 +0800

    fix catppuccin_conf now config style

--- lua/plugins/config/catppuccin_conf.lua
+++ lua/plugins/config/catppuccin_conf.lua
@@ -1,9 +1,11 @@
+vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
+
 require("catppuccin").setup({
 	styles = {
-		comments = "italic",
-		keywords = "bold",
-		functions = "italic",
+		comments = { "italic" },
+		keywords = { "bold" },
+		functions = { "italic" },
 	},
 })
-vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
+
 vim.cmd([[colorscheme catppuccin]])
