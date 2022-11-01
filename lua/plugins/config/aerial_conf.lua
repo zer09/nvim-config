f8d0bd2d2695026ce6a700def7a2340be8b000ed
Gilbert Consellado <gc@C6MM6Q2>
2022-11-01 22:02:08 +0800

    Fix aerial deprecation

--- lua/plugins/config/aerial_conf.lua
+++ lua/plugins/config/aerial_conf.lua
@@ -1,17 +1,9 @@
 local nnoremap = require("helper").nnoremap
 
 require("aerial").setup({
-	close_behavior = "global",
-	placement_editor_edge = "true",
-	on_attach = function(bufnr)
-		local opts = { buffer = bufnr }
-		-- Toggle the aerial window with <Leader>a
-		nnoremap("<Leader>a", "<CMD>AerialToggle!<CR>", opts)
-		-- Jump forwards/backwards with '{' and '}'
-		nnoremap("{", "<CMD>AerialPrev<CR>", opts)
-		nnoremap("}", "<CMD>AerialNext<CR>", opts)
-		-- Jump up the tree with '[[' or ']]'
-		nnoremap("[[", "<CMD>AerialPrevUp<CR>", opts)
-		nnoremap("]]", "<CMD>AerialNextUp<CR>", opts)
-	end,
+	layout = {
+		placement = "edge",
+	},
 })
+
+nnoremap("<Leader>a", "<CMD>AerialToggle!<CR>")
