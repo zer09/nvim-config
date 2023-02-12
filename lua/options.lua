2ca0becef566845ce2b78e4a2f22827c8af5388f
Gilbert Consellado <gilbertconsellado@gmail.com>
2023-02-12 18:36:19 +0800

    wsl clipboard

--- lua/options.lua
+++ lua/options.lua
@@ -29,3 +29,18 @@ set.list = true
 set.pumheight = 10
 
 vim.g.python3_host_prog = "/usr/bin/python3"
+
+if vim.fn.has("wsl") then
+	vim.g.clipboard = {
+		name = "WslClipboard",
+		copy = {
+			["+"] = "clip.exe",
+			["*"] = "clip.exe",
+		},
+		paste = {
+			["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+			["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+		},
+		cache_enabled = true,
+	}
+end
