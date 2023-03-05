a88099f19f4921c7d236c10fcb3088e792bef689
Gilbert Consellado <gilbertconsellado@gmail.com>
2023-03-05 16:24:38 +0800

    update jsonls settings
    enable validate

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -81,7 +81,10 @@ return {
 						on_attach = on_attach,
 						capabilities = capabilities,
 						settings = {
-							json = require("schemastore").json.schemas(),
+							json = {
+								schemas = require("schemastore").json.schemas(),
+								validate = { enable = true },
+							},
 						},
 					})
 				end,
