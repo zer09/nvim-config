d782430bbe916960c222a5fc9be699912d3ed2be
Gilbert Consellado <gilbertconsellado@gmail.com>
2023-09-24 19:16:43 +0800

    refactor(lsp): update lua_ls config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -89,28 +89,42 @@ return {
 						},
 					})
 				end,
-				["lua_ls"] = function(_)
-					local lib = vim.api.nvim_get_runtime_file("", true)
-					table.insert(lib, vim.fn.stdpath("config") .. "/lua")
-					table.insert(lib, "${3rd}/luv/library")
-					table.insert(lib, "${3rd}/luassert/library")
-
+				["lua_ls"] = function()
 					lspconfig.lua_ls.setup({
 						on_attach = on_attach,
 						capabilities = capabilities,
-						settings = {
-							Lua = {
-								runtime = {
-									version = "LuaJIT",
-								},
-								diagnostics = {
-									globals = { "vim" },
-								},
-								workspace = {
-									library = lib,
-								},
-							},
-						},
+						on_init = function(client)
+							local path = client.workspace_folders[1].name
+							if
+								not vim.loop.fs_stat(path .. "/.luarc.json")
+								and not vim.loop.fs_stat(path .. "/.luarc.jsonc")
+							then
+								client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
+									Lua = {
+										runtime = {
+											-- Tell the language server which version of Lua you're using
+											-- (most likely LuaJIT in the case of Neovim)
+											version = "LuaJIT",
+										},
+										-- Make the server aware of Neovim runtime files
+										workspace = {
+											checkThirdParty = false,
+											library = {
+												vim.env.VIMRUNTIME,
+												-- "${3rd}/luv/library"
+												-- "${3rd}/busted/library",
+											},
+											-- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+											-- library = vim.api.nvim_get_runtime_file("", true)
+										},
+									},
+								})
+
+								client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+							end
+
+							return true
+						end,
 					})
 				end,
 				["tsserver"] = function(_)
