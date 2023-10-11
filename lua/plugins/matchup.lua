c71db47e17d14a45d375c3e6a86153c5e9e897ab
Gilbert Consellado <gilbertconsellado@gmail.com>
2023-10-11 21:17:03 +0800

    fix(plugins): match offscreen method popup bug

--- lua/plugins/matchup.lua
+++ lua/plugins/matchup.lua
@@ -3,7 +3,8 @@ return {
 	version = false,
 	event = "VeryLazy",
 	config = function()
-		vim.g.matchup_matchparen_offscreen = { method = "popup" }
+		-- vim.g.matchup_matchparen_offscreen = { method = "popup" }
+		vim.g.matchup_matchparen_offscreen = {}
 		vim.g.matchup_matchparen_deferred = 1
 		vim.g.matchup_motion_override_Npercent = 0 -- enable vim default {count}%
 	end,
