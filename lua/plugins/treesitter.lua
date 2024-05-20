c3abf46c6a81ad8b496e8a4c0da66a102f97a329
Gilbert Consellado <gilbertconsellado@gmail.com>
2024-05-20 16:24:01 +0800

    chore(update): windwp/nvim-ts-autotag treesitter deprecation

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,65 +1,70 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	version = false,
-	build = ":TSUpdate",
-	event = "VeryLazy",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter-context",
+	{
 		"windwp/nvim-ts-autotag",
-		"andymass/vim-matchup",
+		event = "VeryLazy",
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
 	},
-	opts = {
-		highlight = {
-			enable = true,
-			disable = { "fzf" },
-			additional_vim_regex_highlighting = { "python" },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		version = false,
+		build = ":TSUpdate",
+		event = "VeryLazy",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-context",
+			"andymass/vim-matchup",
 		},
-		ensure_installed = {
-			"bash",
-			"comment",
-			"css",
-			"dart",
-			"go",
-			"hjson",
-			"html",
-			"javascript",
-			"jsonc",
-			"jsdoc",
-			"lua",
-			"markdown",
-			"markdown_inline",
-			"proto",
-			"query",
-			"regex",
-			"rust",
-			"scss",
-			"svelte",
-			"toml",
-			"typescript",
-			"vim",
-			"vimdoc",
-			"yaml",
-		},
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<C-Space>",
-				node_incremental = "<C-Space>",
-				scope_incremental = false,
-				node_decremental = "<BS>",
+		opts = {
+			highlight = {
+				enable = true,
+				disable = { "fzf" },
+				additional_vim_regex_highlighting = { "python" },
+			},
+			ensure_installed = {
+				"bash",
+				"comment",
+				"css",
+				"dart",
+				"go",
+				"hjson",
+				"html",
+				"javascript",
+				"jsonc",
+				"jsdoc",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"proto",
+				"query",
+				"regex",
+				"rust",
+				"scss",
+				"svelte",
+				"toml",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"yaml",
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-Space>",
+					node_incremental = "<C-Space>",
+					scope_incremental = false,
+					node_decremental = "<BS>",
+				},
+			},
+			matchup = {
+				enable = true,
 			},
 		},
-		autotag = {
-			enable = true,
-		},
-		matchup = {
-			enable = true,
-		},
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
+			require("treesitter-context").setup({
+				enable = true,
+			})
+		end,
 	},
-	config = function(_, opts)
-		require("nvim-treesitter.configs").setup(opts)
-		require("treesitter-context").setup({
-			enable = true,
-		})
-	end,
 }
