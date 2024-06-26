f3a5123ec2b6c111eceb5a024017f57578d495fd
Gilbert Consellado <gilbertconsellado@gmail.com>
2024-06-26 23:47:43 +0800

    refactor(neogit): opening neogit use replace instead of new tab

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -16,6 +16,7 @@ return {
 			disable_commit_confirmation = false,
 			disable_builtin_notifications = true,
 			disable_insert_on_commit = false,
+			kind = "replace",
 			signs = {
 				-- { CLOSED, OPENED }
 				section = { "", "" },
