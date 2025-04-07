cc6462d231a836f28e879f83cf67a954de115d69
Gilbert Consellado <gilbertconsellado@gmail.com>
2025-04-07 12:14:49 +0800

    fix(telescope): fzf telescope revert to make

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,7 +8,7 @@ return {
 			"nvim-tree/nvim-web-devicons",
 			{
 				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+				build = "make",
 			},
 		},
 		config = function()
