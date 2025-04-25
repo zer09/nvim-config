8d3828227a05727ec5e099c41a8eaf21ce64a6c5
Gilbert Consellado <gilbertconsellado@gmail.com>
2025-04-25 10:38:13 +0800

    replace choosewin with nvim-window

--- lua/goto.lua
+++ lua/goto.lua
@@ -1,9 +1,11 @@
 local function goto_definition()
+	print(vim.inspect("test 1"))
 	local util = vim.lsp.util
 	local log = require("vim.lsp.log")
 	local api = vim.api
 
 	local handler = function(_, result, ctx)
+		print(vim.inspect("test 2"))
 		if result == nil or vim.tbl_isempty(result) then
 			local _ = log.info() and log.info(ctx.method, "No location found")
 			return nil
@@ -13,7 +15,8 @@ local function goto_definition()
 			vim.cmd("vsplit")
 		end
 
-		util.jump_to_location(result[1], "utf-8")
+		-- util.jump_to_location(result[1], "utf-8")
+		vim.lsp.util.show_document(result[1], "utf-8", { focus = true })
 
 		if #result > 1 then
 			util.set_qflist(util.locations_to_items(result, "utf-8"))
--- lua/plugins/choosewin.lua
+++ lua/plugins/choosewin.lua
@@ -1,19 +1,47 @@
 return {
+	-- {
+	-- 	"t9md/vim-choosewin",
+	-- 	version = false,
+	-- 	event = "VeryLazy",
+	-- 	config = function()
+	-- 		-- Label to choose when selecting tab.
+	-- 		vim.g.choosewin_label = "HTNSAOEU"
+	--
+	-- 		-- Dont show labels when there are only single window.
+	-- 		vim.g.choosewin_return_on_single_win = 1
+	--
+	-- 		-- Dont blink the cursor window is selected.
+	-- 		vim.g.choosewin_blink_on_land = 0
+	--
+	-- 		-- require("helper").nmap("-", "<Plug>(choosewin)")
+	-- 	end,
+	-- },
+	-- {
+	-- 	-- WINDOW PICKER
+	-- 	"s1n7ax/nvim-window-picker",
+	-- 	version = "v1.*",
+	-- 	config = function()
+	-- 		local picker = require("window-picker")
+	-- 		picker.setup({ fg_color = "#000000", hint = "floating-big-letter" })
+	--
+	-- 		vim.keymap.set("n", "<leader><leader>w", function()
+	-- 			local picked_window_id = picker.pick_window() or vim.api.nvim_get_current_win()
+	-- 			vim.api.nvim_set_current_win(picked_window_id)
+	-- 		end, { desc = "Pick a window" })
+	-- 	end,
+	-- },
 	{
-		"t9md/vim-choosewin",
-		version = false,
-		event = "VeryLazy",
+		"yorickpeterse/nvim-window",
+		-- keys = {
+		-- 	{ "<leader>wj", "<cmd>lua require('nvim-window').pick()<cr>", desc = "nvim-window: Jump to window" },
+		-- },
 		config = function()
-			-- Label to choose when selecting tab.
-			vim.g.choosewin_label = "HTNSAOEU"
+			require("nvim-window").setup({
+				-- The characters available for hinting windows.
+				chars = { "h", "t", "n", "s", "a", "o", "e", "u" },
+			})
 
-			-- Dont show labels when there are only single window.
-			vim.g.choosewin_return_on_single_win = 1
-
-			-- Dont blink the cursor window is selected.
-			vim.g.choosewin_blink_on_land = 0
-
-			require("helper").nmap("-", "<Plug>(choosewin)")
+			require("helper").nmap("-", "<CMD>lua require('nvim-window').pick()<CR>")
 		end,
 	},
 }
