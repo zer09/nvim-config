1d268f319d25fcaa2f1ae0ac62248d8fa964ca80
Gilbert Consellado <gilbertconsellado@gmail.com>
2025-07-13 10:56:39 +0800

    refactor(lualine): change the section seperator

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,7 +5,7 @@ return {
 		opts = {
 			options = {
 				component_separators = "",
-				section_separators = "",
+				section_separators = { left = "", right = "" },
 			},
 			sections = {
 				lualine_a = {
