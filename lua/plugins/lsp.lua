3c3a8f9ed75eaf0541c593434e31a9953983a450
Gilbert Consellado <gilbertconsellado@gmail.com>
2025-09-11 11:44:54 +0800

    lsp color

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -45,18 +45,12 @@ return {
 	{
 		"saghen/blink.cmp",
 		version = "1.*",
+		dependencies = {
+			"onsails/lspkind.nvim",
+			-- "xzbdmw/colorful-menu.nvim",
+		},
 		opts = {
 			keymap = { preset = "enter" },
-			completions = {
-				documentation = {
-					auto_show = true,
-					auto_show_delay_ms = 500,
-					treesitter_highlighting = true,
-				},
-				list = {
-					selection = { preselect = false, auto_insert = true },
-				},
-			},
 			sources = {
 				default = { "lazydev", "lsp", "path", "snippets", "buffer" },
 				providers = {
@@ -68,6 +62,70 @@ return {
 					},
 				},
 			},
+			appearance = {
+				use_nvim_cmp_as_default = true,
+			},
+			completion = {
+				documentation = {
+					auto_show = true,
+				},
+				ghost_text = {
+					enabled = true,
+				},
+				menu = {
+					border = "rounded",
+					draw = {
+						columns = { { "kind_icon" }, { "label" }, { "kind" }, { "source_name" } },
+						components = {
+							-- label = {
+							-- 	text = function(ctx)
+							-- 		return require("colorful-menu").blink_components_text(ctx)
+							-- 	end,
+							-- 	highlight = function(ctx)
+							-- 		return require("colorful-menu").blink_components_highlight(ctx)
+							-- 	end,
+							-- },
+							kind_icon = {
+								text = function(ctx)
+									local icon = ctx.kind_icon
+									if vim.tbl_contains({ "Path" }, ctx.source_name) then
+										local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+										if dev_icon then
+											icon = dev_icon
+										end
+									else
+										icon = require("lspkind").symbolic(ctx.kind, {
+											mode = "symbol",
+										})
+									end
+
+									return icon .. ctx.icon_gap
+								end,
+
+								-- Optionally, use the highlight groups from nvim-web-devicons
+								-- You can also add the same function for `kind.highlight` if you want to
+								-- keep the highlight groups in sync with the icons.
+								highlight = function(ctx)
+									local hl = ctx.kind_hl
+									if vim.tbl_contains({ "Path" }, ctx.source_name) then
+										local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+										if dev_icon then
+											hl = dev_hl
+										end
+									end
+									return hl
+								end,
+							},
+							source_name = {
+								text = function(ctx)
+									return "[" .. ctx.source_name .. "]"
+								end,
+							},
+						},
+					},
+				},
+			},
+			signature = { enabled = true },
 		},
 	},
 	{
