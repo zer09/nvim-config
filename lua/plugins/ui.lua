e7270604f5a109573b195fa3c45a7895ecc77679
Gilbert Consellado <gilbertconsellado@gmail.com>
2025-10-04 21:07:20 +0800

    feat(ui): disable animation on winsep

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -113,7 +113,13 @@ return {
 	},
 	{
 		"nvim-zh/colorful-winsep.nvim",
-		config = true,
 		event = { "WinLeave" },
+		config = function()
+			require("colorful-winsep").setup({
+				animate = {
+					enabled = false,
+				},
+			})
+		end,
 	},
 }
