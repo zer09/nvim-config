3f6786f19e86ec76dc75c0c49faef46524901a40
Gilbert Consellado <gilbertconsellado@gmail.com>
2025-10-13 00:28:01 +0800

    feat(ui): add mini jump and setup leap

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -31,6 +31,23 @@ return {
 		"ggandor/leap.nvim",
 		config = function()
 			require("leap").add_default_mappings()
+			vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)")
+			-- vim.keymap.set("n", "S", "<Plug>(leap-from-window)")
+
+			local leap = require("leap")
+
+			leap.opts.preview_filter = function(ch0, ch1, ch2)
+				return not (ch1:match("%s") or ch0:match("%a") and ch1:match("%a") and ch2:match("%a"))
+			end
+
+			leap.opts.equivalence_classes = { " \t\r\n", "([{", ")]}", "'\"`" }
+		end,
+	},
+	{
+		"nvim-mini/mini.jump",
+		version = false,
+		config = function()
+			require("mini.jump").setup()
 		end,
 	},
 	{
