1f538c601a55aa552f9881ddc155759a0cf77aa0
Gilbert Consellado <gilbertconsellado@gmail.com>
2025-10-27 10:42:37 +0800

    refactor(lsp): only enable lazydev if init.lua is found

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -102,6 +102,9 @@ return {
 				-- Load luvit types when the `vim.uv` word is found
 				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
 			},
+			enabled = function(root_dir)
+				return not vim.uv.fs_stat(root_dir .. "/.init.lua")
+			end,
 		},
 	},
 	{
