3cae89f3db07e3f35df05ced8d066469e2f30f2f
Gilbert Consellado <gilbertconsellado@gmail.com>
2025-11-16 17:33:05 +0800

    chore(ui): remove deprecated leap default mapping

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -30,7 +30,7 @@ return {
 	{
 		"ggandor/leap.nvim",
 		config = function()
-			require("leap").add_default_mappings()
+			-- require("leap").add_default_mappings()
 			vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)")
 			-- vim.keymap.set("n", "S", "<Plug>(leap-from-window)")
 
