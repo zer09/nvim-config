faee2ad09becb0215cfe60f7d07ee250b8d587e6
Gilbert Consellado <gilbertconsellado@gmail.com>
2026-03-30 01:21:15 +0800

    refactor(plugins): update ruff config for django

--- after/lsp/ruff.lua
+++ after/lsp/ruff.lua
@@ -1,9 +1,12 @@
 vim.lsp.config("ruff", {
 	init_options = {
 		settings = {
+			lineLength = 88,
 			configurationPreference = "filesystemFirst",
 			lint = {
 				preview = true,
+				select = { "E", "W", "F", "I", "B", "UP", "DJ", "SIM", "RUF" },
+				ignore = { "E501", "B008" },
 			},
 		},
 	},
